549c93124dd606778acb914e81c861d2fd9b3c52
Your Name <you@example.com>
2022-05-18 16:57:29 +0500

    second commit

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,13 +1,9 @@
 import os
 import socketio
-
-from django.core.wsgi import get_wsgi_application
 from moderators.views import sio
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-
 application = socketio.WSGIApp(sio, application)
